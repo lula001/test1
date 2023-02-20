5177bc99fed1e948e6b1386902c9da011307b28f
lula001 <lula001@ess.ais.ac.nz>
2023-02-20 14:30:37 +1300

    abc

---
+++ project.java
@@ -0,0 +1,12 @@
+public class Test
+{
+	public static void main(String[] args)
+	{
+
+	int tableNo=0;
+
+	for(int i=1; i<= 5; i++)
+		System.out.println(tableNo + "*" + i + " = " + tableNo * i);
+System.out.println("hello");
+	}
+}
